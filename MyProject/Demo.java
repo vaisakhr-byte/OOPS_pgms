00e7c7b59f78ac308485f01322a9085d411164c6
vaisakhr-byte <rajeshvaisakh5@gmail.com>
2025-08-16 13:18:32 +0530

    package

---
+++ MyProject/Demo.java
@@ -0,0 +1,13 @@
+// File: MyProject/demo/Demo.java
+
+package demo;
+
+public class Demo {
+    // A public member variable
+    public String message = "Hello from the Demo class in another package!";
+
+    // A public method
+    public void displayMessage() {
+        System.out.println("This method is called from outside the 'demo' package.");
+    }
+}
